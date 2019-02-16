d335079c54691a3dcad24ccc87eabf98437a57e4
soranetabata <21331085ST@gmail.com>
2019-02-16 08:53:40 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,70 +1,76 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update] 
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: :destroy
-   def index
-    @users = User.where(activated: FILL_IN).paginate(page: params[:page])
-   end
-   
-   def show
-    @user = User.find(params[:id])
-    redirect_to root_url and return unless FILL_IN
-   end
-
-  def new
-      @user = User.new
+  
+  def index
+    #@users = User.all
+    @users = User.paginate(page: params[:page])
   end
   
-   def create
-     @user = User.new(user_params)
+  def new
+    @user = User.new
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+    # debugger
+  end
+  
+  def create
+    @user = User.new(user_params)
     if @user.save
-     @user.send_activation_email
+      # success
+      @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
       redirect_to root_url
+      #log_in @user
+      #flash[:success] = "Welcome to the Sample App!"
+      #redirect_to @user
     else
+      # failure
       render 'new'
     end
-   end
-   
-   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User deleted"
-    redirect_to users_url
-   end
-   
-   def edit
+  end
+  
+  def edit
     @user = User.find(params[:id])
-   end
-   
-    def update
+  end
+  
+  def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
+      # Handle a successful update.
       flash[:success] = "Profile updated"
       redirect_to @user
     else
       render 'edit'
     end
-    end
-   private
-
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+  
+  private
+  
     def user_params
-      params.require(:user).permit(:name, :email, :password,
+      params.require(:user).permit(:name,
+                                   :email,
+                                   :password,
                                    :password_confirmation)
     end
     
-     # ログイン済みユーザーかどうか確認
-    def logged_in_user
-      unless logged_in?
-       store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-    
+    # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
+      redirect_to(root_url) unless current_user? @user
     end
+    
+    # Confirms an admin user.
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
